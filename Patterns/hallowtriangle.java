2576a9a5f9e5c35b3dbdc7ef15e243d61db4377f
Manoj R gowda <160026926+Manojrgowdaa@users.noreply.github.com>
2024-07-08 12:39:43 +0530

    Add files via upload

---
+++ Patterns/hallowtriangle.java
@@ -0,0 +1,22 @@
+class hallowtriangle{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if(j==1||i==n||i==j){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
+
+// *
+// **
+// * *
+// *  *
+// *****
---
+++ Patterns/pattern6.java
@@ -0,0 +1,19 @@
+ class pattern5 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 1, 1, 1, 1},
+            {1, 0, 1, 1, 0},
+            {1, 1, 0, 1, 1},
+            {1, 0, 1, 0, 1},
+            {1, 1, 1, 1, 0}
+        };
+        
+        // Print the matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
