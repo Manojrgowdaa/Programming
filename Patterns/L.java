89685681c4f41ac4d3b043d3c6a485d07c413749
Manoj R gowda <160026926+Manojrgowdaa@users.noreply.github.com>
2024-07-08 15:48:18 +0530

    Add files via upload

---
+++ Patterns/L.java
@@ -0,0 +1,14 @@
+class L{
+    public static void lshape(){
+        int n=5;
+        for(int i=0;i<n-1;i++){
+            System.out.println("*");
+
+        }for(int i=0;i<n;i++){
+            System.out.print("* ");
+        }
+    }
+    public static void main(String[] args) {
+        lshape();
+    }
+}
---
+++ Patterns/Lpattern.java
@@ -0,0 +1,16 @@
+class Lpattern{
+    public static void main(String[] args) {
+        int height=5;
+        for(int i=0;i<=height;i++){
+        if(i==height-1){
+          for(int j=0;j<height-1;j++){
+            System.out.print("* ");
+          }
+        }else{
+            System.out.println("* ");
+
+        }
+
+    }
+}
+}
